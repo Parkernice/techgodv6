48f0030dd91a6236731172e6535b339733b701aa
Parkernice <parkernice888@gmail.com>
2024-09-25 17:54:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,19 +11,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "916378704507"
-global.ownername = "AASHISH"
+global.ownernomer = "2349168023704"
+global.ownername = "Parkerlee"
 global.ytname = "YT: SHADOW STARCORE"
 global.socialm = "GitHub: dark-tech-zone"
 global.location = "India, Delhi, city"
 
-global.ownernumber = '916378704507'  //creator number
-global.ownername = 'AASHISH' //owner name
+global.ownernumber = '2349168023704'  //creator number
+global.ownername = 'Parker lee' //owner name
 global.botname = 'AASHISH ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'AASHISH\n\nContact: +919310763037'
+global.author = 'AASHISH\n\nContact: +2349168023704'
 
 //console view/theme
 global.themeemoji = '🪀'
@@ -38,7 +38,7 @@ global.prefa = ['.']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
